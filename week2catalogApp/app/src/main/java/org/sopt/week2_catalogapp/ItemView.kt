9ba727f7d7c4aa05bcf66bbe0979a5a8bfb6b30e
kangyuri1114 <yuri3334@gmail.com>
2023-11-08 04:10:57 +0900

    [week2] 패딩 수정

--- week2catalogApp/app/src/main/java/org/sopt/week2_catalogapp/ItemView.kt
+++ week2catalogApp/app/src/main/java/org/sopt/week2_catalogapp/ItemView.kt
@@ -1,12 +1,16 @@
 package org.sopt.week2_catalogapp
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,8 +25,9 @@ import org.sopt.week2_catalogapp.ui.theme.Week2catalogAppTheme
 
 @Composable
 fun ItemCard(data: Data) {
-    Column(
-        modifier = Modifier.wrapContentSize(),
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp),
+        border = BorderStroke(2.dp, color = Color.DarkGray),
     ) {
         ItemImage(
             imageResource = data.image,
@@ -37,6 +42,7 @@ fun ItemCard(data: Data) {
 fun ItemImage(imageResource: Int, content: String) {
     Column(
         verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier.padding(10.dp)
     ) {
         val painter =
             rememberAsyncImagePainter(
@@ -55,17 +61,16 @@ fun ItemImage(imageResource: Int, content: String) {
     }
 }
 
-
 @Composable
 fun ItemTitle(title: String) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.wrapContentWidth(),
+        modifier = Modifier.wrapContentWidth().padding(horizontal = 5.dp),
     ) {
         Text(
             text = title,
             modifier =
-            Modifier.padding(10.dp),
+            Modifier.padding(horizontal = 10.dp),
             style = MaterialTheme.typography.headlineSmall,
         )
     }
@@ -75,7 +80,7 @@ fun ItemTitle(title: String) {
 fun ItemContent(content: String) {
     Column(
         verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier.wrapContentWidth(),
+        modifier = Modifier.wrapContentWidth().padding(horizontal = 5.dp),
 
     ) {
         Text(
@@ -92,35 +97,6 @@ fun ItemContent(content: String) {
 @Composable
 fun PreviewItem() {
     Week2catalogAppTheme {
-        val dataList =
-            listOf(
-                Data(
-                    title = "Title 1",
-                    content = "Description 1",
-                    image = R.drawable.img_ham1,
-                ),
-                Data(
-                    title = "Title 2",
-                    content = "Description 2",
-                    image = R.drawable.img_ham2,
-                ),
-                Data(
-                    title = "Title 3",
-                    content = "Description 3",
-                    image = R.drawable.img_ham3,
-                ),
-                Data(
-                    title = "Title 4",
-                    content = "Description 4",
-                    image = R.drawable.img_ham4,
-                ),
-                Data(
-                    title = "Title 5",
-                    content = "Description 5",
-                    image = R.drawable.img_ham5,
-                ),
-            )
-
         dataList.forEach { data ->
             ItemCard(data)
         }
