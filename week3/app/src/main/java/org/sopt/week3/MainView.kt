b1b9e41df49f1065c2ef0f21c87223047e906d0f
kangyuri1114 <yuri3334@gmail.com>
2023-11-15 03:08:26 +0900

    [Assignment/week3] 프로그래스바 내부에 텍스트 넣기

--- week3/app/src/main/java/org/sopt/week3/MainView.kt
+++ week3/app/src/main/java/org/sopt/week3/MainView.kt
@@ -1,5 +1,6 @@
 package org.sopt.week3
 
+import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,6 +19,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +35,7 @@ fun SurveyScreen() {
     Column {
         SurveyBtn(showDialog)
         SurveyDialog(showDialog)
-        ScoreCanvas()
+        ScoreCanvas(10)
     }
 }
 
@@ -41,7 +44,8 @@ fun SurveyScreen() {
 fun SurveyBtn(showDialog: MutableState<Boolean>) {
     Column(
         modifier = Modifier
-            .padding(vertical = 8.dp).fillMaxWidth(),
+            .padding(vertical = 8.dp)
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.Bottom,
     ) {
         Button(
@@ -75,7 +79,7 @@ fun TotalScore(score: Int) {
 }
 
 @Composable
-fun ScoreCanvas() {
+fun ScoreCanvas(score: Int) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -83,18 +87,45 @@ fun ScoreCanvas() {
     ) {
         Canvas(modifier = Modifier.fillMaxSize()) {
             val sizeArc = size / 1.5F
+            val center = Offset(size.width / 2f, size.height / 2f)
 
             drawArc(
                 color = Color.Gray,
-                startAngle = 0f,
-                sweepAngle = -180f,
-                useCenter = false,
+                startAngle = -180f, // 시작 각도
+                sweepAngle = 180f, // 돌아갈 각도
+                useCenter = false, // 호가 경계 중심부에 닿는가
                 topLeft = Offset(
                     (size.width - sizeArc.width) / 2f,
                     (size.height - sizeArc.height) / 2f,
                 ),
                 size = sizeArc,
                 style = Stroke(width = 25f),
+            )
+            val textPosition1 = center + Offset(-0f, -150f)
+            val textPosition2 = center + Offset(-0f, -40f)
+
+            drawContext.canvas.nativeCanvas.drawText(
+                "지금 내 점수는",
+                textPosition1.x,
+                textPosition1.y,
+                Paint().apply {
+                    color = Color.Gray.toArgb()
+                    textSize = 40f
+                    textAlign = Paint.Align.CENTER
+                    isFakeBoldText = true
+                },
+            )
+
+            drawContext.canvas.nativeCanvas.drawText(
+                score.toString(),
+                textPosition2.x,
+                textPosition2.y,
+                Paint().apply {
+                    color = Color.Red.toArgb()
+                    textSize = 100f
+                    textAlign = Paint.Align.CENTER
+                    isFakeBoldText = true
+                },
             )
         }
     }
@@ -104,6 +135,6 @@ fun ScoreCanvas() {
 @Composable
 fun MainPreview() {
     Week3Theme {
-        ScoreCanvas()
+        ScoreCanvas(10)
     }
 }
